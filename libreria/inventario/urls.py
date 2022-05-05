831c7e4e5870459c9ab4c515bef6fb05832d985e
Jose Valencia <19josevalencia97@gmail.com>
2022-05-05 06:23:15 -0400

    lista de autores

--- libreria/inventario/urls.py
+++ libreria/inventario/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('autores', views.autores, name='autores')
 ]
--- libreria/inventario/views.py
+++ libreria/inventario/views.py
@@ -6,3 +6,15 @@ def index(request):
     data = dict()
     data['titulo'] = "Inventario El Poeta"
     return render(request, template, data)
+
+def autores(request): 
+    template = "autores/lista.html"
+    data = dict()
+    data['titulo'] = "Autores Registrados"
+    info = dict()
+    autores = []
+    autores.append({'nombre': "Jose", 'apellido': "Valencia", 'correo': "jose.valencia@gmail.com"})
+    autores.append({'nombre': "Juan", 'apellido': "Vasquez", 'correo': "juan.vasquez@gmail.com"})
+    autores.append({'nombre': "Javiera", 'apellido': "Simpson", 'correo': "javiera.Simpson@gmail.com"})
+    data['autores'] = autores
+    return render(request, template, data)
