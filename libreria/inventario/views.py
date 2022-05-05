402bb24a82dd6c10f5911532003dddef2a590c05
Jose Valencia <19josevalencia97@gmail.com>
2022-05-05 05:35:32 -0400

    creación nueva aplicación

--- libreria/inventario/views.py
+++ libreria/inventario/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    template = 'inventario/index.html'
+    data = dict()
+    data['titulo'] = "Inventario El Poeta"
+    return render(request, template, data)
