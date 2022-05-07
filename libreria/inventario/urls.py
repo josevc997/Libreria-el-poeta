7d1f998feea398941c0a64d810c674e71bfb7742
Jose Valencia <19josevalencia97@gmail.com>
2022-05-07 03:37:24 -0400

    lista y registro productos

--- libreria/inventario/urls.py
+++ libreria/inventario/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('autores', views.autores, name='autores'),
     path('autores/registro', views.registroAutores, name='registroAutores'),
+    path('productos', views.productos, name='productos'),
+    path('productos/registro', views.registroProductos, name='registroProductos'),
 ]
--- libreria/inventario/views.py
+++ libreria/inventario/views.py
@@ -30,3 +30,34 @@ def registroAutores(request):
         apellido = request.POST['apellido']
 
     return render(request, template, data)
+
+def productos(request): 
+    template = "productos/lista.html"
+    data = dict()
+    data['titulo'] = "productos Registrados"
+    info = dict()
+    productos = []
+    productos.append({'nombre': "primer libro", 'tipo': "Revista", 'annio': 2015, 'edicion': 'Primera Edición', 'resumen': "Mejor libro de todos"})
+    productos.append({'nombre': "segundo libro", 'tipo': "libro", 'annio': 1997, 'edicion': 'Segunda Edición', 'resumen': "Segundo mejor libro de todos"})
+    productos.append({'nombre': "tercer libro", 'tipo': "enciclopedia", 'annio': 2022, 'edicion': 'Ultima Edición', 'resumen': "Peor libro de todos"})
+    data['productos'] = productos
+    return render(request, template, data)
+
+def registroProductos(request):
+    template = "productos/registro.html"
+    data = dict()
+    data['titulo'] = "Registro Productos"
+    data['autores'] = [{'nombre': 'Neftali Reyes', 'id': 1},{'nombre': 'Violeta Parra', 'id': 2},{'nombre': 'Nicanor Parra', 'id': 3}]
+    data['editoriales'] = [{'nombre': 'Zig-Zag', 'id': 1},{'nombre': 'Alfaguara', 'id': 2},{'nombre': 'Libros Premiere', 'id': 3}]
+
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        tipo = request.POST['tipo']
+        annio = request.POST['annio']
+        edicion = request.POST['edicion']
+        resumen = request.POST['resumen']
+        autor = request.POST['autor']
+
+        print(autor)
+
+    return render(request, template, data)
