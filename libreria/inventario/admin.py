a514e5d70c5dde284d8f29150aa2f26679bd79bf
Jose Valencia <19josevalencia97@gmail.com>
2022-06-06 06:42:48 -0400

    nuevos modelos

--- libreria/inventario/admin.py
+++ libreria/inventario/admin.py
@@ -1,5 +1,66 @@
 from django.contrib import admin
-from .models import Persona
+from .models import Bodega, Compra, Editorial, Genero, Genero_Publicacion, Movimiento, Pedido, Perfil, Persona, Proveedor, Publicacion, Autor, Autor_Publicacion, Publicacion_Bodega, Publicacion_Compra, Publicacion_Movimiento, Publicacion_Pedido
+
+class Autor_Publicacion_Inline(admin.StackedInline):
+    model = Autor_Publicacion
+    extra = 1
+
+class Genero_Publicacion_Inline(admin.StackedInline):
+    model = Genero_Publicacion
+    extra = 1
+
+class Publicacion_Bodega_Inline(admin.StackedInline):
+    model = Publicacion_Bodega
+    extra = 1
+
+class Publicacion_Compra_Inline(admin.StackedInline):
+    model = Publicacion_Compra
+    extra = 1
+
+class Publicacion_Movimiento_Inline(admin.StackedInline):
+    model = Publicacion_Movimiento
+    extra = 1
+
+class Publicacion_Pedido_Inline(admin.StackedInline):
+    model = Publicacion_Pedido
+    extra = 1
+
+class Pedido_Admin(admin.ModelAdmin):
+    inlines = (Publicacion_Pedido_Inline,)
+
+class Movimiento_Admin(admin.ModelAdmin):
+    inlines = (Publicacion_Movimiento_Inline,)
+
+class Compra_Admin(admin.ModelAdmin):
+    inlines = (Publicacion_Compra_Inline,)
+
+class Bodega_Admin(admin.ModelAdmin):
+    inlines = (Publicacion_Bodega_Inline,)
+
+class Genero_Admin(admin.ModelAdmin):
+    inlines = (Genero_Publicacion_Inline,)
+
+class Autor_Admin(admin.ModelAdmin):
+    inlines = (Autor_Publicacion_Inline,)
+
+class Publicacion_Admin(admin.ModelAdmin):
+    inlines = (Autor_Publicacion_Inline, Genero_Publicacion_Inline, Publicacion_Bodega_Inline, Publicacion_Compra_Inline, Publicacion_Movimiento_Inline, Publicacion_Pedido_Inline, )
 
 # Register your models here.
+admin.site.register(Autor, Autor_Admin)
+admin.site.register(Genero, Genero_Admin)
+admin.site.register(Editorial)
+admin.site.register(Publicacion, Publicacion_Admin)
+admin.site.register(Genero_Publicacion)
+admin.site.register(Autor_Publicacion)
+admin.site.register(Bodega, Bodega_Admin)
+admin.site.register(Publicacion_Bodega)
 admin.site.register(Persona)
+admin.site.register(Perfil)
+admin.site.register(Proveedor)
+admin.site.register(Pedido, Pedido_Admin)
+admin.site.register(Publicacion_Pedido)
+admin.site.register(Compra, Compra_Admin)
+admin.site.register(Publicacion_Compra)
+admin.site.register(Movimiento, Movimiento_Admin)
+admin.site.register(Publicacion_Movimiento)
--- libreria/inventario/models.py
+++ libreria/inventario/models.py
@@ -14,6 +14,7 @@ class Autor(models.Model):
 
     class Meta:
         db_table = "autor"
+        verbose_name_plural = "autores"
         managed = False
 
 class Genero(models.Model):
@@ -25,6 +26,7 @@ class Genero(models.Model):
 
     class Meta:
         db_table = "genero"
+        verbose_name_plural = "generos"
         managed = False
 
 class Editorial(models.Model):
@@ -32,12 +34,14 @@ class Editorial(models.Model):
     nombre_editorial = models.CharField(max_length=30)
     correo_editorial = models.CharField(max_length=30, null=True)
     telefono_editorial = models.CharField(max_length=30, null=True)
+    direccion_editorial = models.CharField(max_length=30, null=True)
 
     def __str__(self):
         return u'%s' % (self.nombre_editorial)
 
     class Meta:
         db_table = "editorial"
+        verbose_name_plural = "editoriales"
         managed = False
 
 class Publicacion(models.Model):
@@ -51,12 +55,27 @@ class Publicacion(models.Model):
     isbn = models.CharField(max_length=30)
     numero_serie = models.IntegerField()
     precio = models.IntegerField()
+    autores = models.ManyToManyField(Autor, through='Autor_Publicacion')
+    generos = models.ManyToManyField(Genero, through='Genero_Publicacion')
 
     def __str__(self):
         return u'%s' % (self.nombre)
 
     class Meta:
         db_table = "publicacion"
+        verbose_name_plural = "publicaciones"
+        managed = False
+
+class Genero_Publicacion(models.Model):
+    id_genero = models.ForeignKey(Genero, on_delete=models.CASCADE, db_column='id_genero')
+    id_publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, db_column='id_publicacion')
+
+    # def __str__(self):
+    #     return u'%s' % (self.id_publicacion.nombre + " escrito por" + self.id_genero.pseudonimo_genero)
+
+    class Meta:
+        db_table = "genero_publicacion"
+        verbose_name_plural = "genero_publicaciones"
         managed = False
 
 class Autor_Publicacion(models.Model):
@@ -68,6 +87,36 @@ class Autor_Publicacion(models.Model):
 
     class Meta:
         db_table = "autor_publicacion"
+        verbose_name_plural = "autor_publicaciones"
+        managed = False
+
+class Bodega(models.Model):
+    id_bodega = models.AutoField(primary_key=True)
+    nombre_bodega = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    comuna = models.CharField(max_length=30)
+    telefono_bodega = models.CharField(max_length=30)
+    publicaciones = models.ManyToManyField(Publicacion, through='Publicacion_Bodega')
+    
+    def __str__(self):
+        return u'%s' % (self.nombre_bodega)
+
+    class Meta:
+        db_table = "bodega"
+        verbose_name_plural = "bodegas"
+        managed = False
+
+class Publicacion_Bodega(models.Model):
+    id_bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE, db_column='id_bodega')
+    id_publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, db_column='id_publicacion')
+    cantidad = models.IntegerField(null=True)
+    
+    # def __str__(self):
+    #     return u'%s' % (self.id_publicacion.nombre + " escrito por" + self.id_autor.pseudonimo_autor)
+
+    class Meta:
+        db_table = "publicacion_bodega"
+        verbose_name_plural = "publicacion_bodegas"
         managed = False
 
 class Persona(models.Model):
@@ -84,4 +133,131 @@ class Persona(models.Model):
 
     class Meta:
         db_table = "persona"
+        verbose_name_plural = "personas"
+        managed = False
+
+class Perfil(models.Model):
+    id_perfil = models.AutoField(primary_key=True)
+    id_persona = models.ForeignKey(Persona, on_delete=models.CASCADE, db_column='id_persona')
+    id_bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE, db_column='id_bodega')
+    nombre_usuario = models.CharField(max_length=30)
+    clave = models.CharField(max_length=30)
+    tipo_usuario = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return u'%s' % (self.nombre_usuario)
+    
+    class Meta:
+        db_table = "perfil"
+        verbose_name_plural = "perfiles"
+        managed = False
+
+
+class Proveedor(models.Model):
+    id_proveedor = models.AutoField(primary_key=True)
+    nombre_proveedor = models.CharField(max_length=30, null=True)
+    direccion_proveedor = models.CharField(max_length=30, null=True)
+    comuna_proveedor = models.CharField(max_length=30, null=True)
+    correo_proveedor = models.CharField(max_length=30, null=True)
+    telefono_proveedor = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return u'%s' % (self.nombre_proveedor)
+
+    class Meta:
+        db_table = "proveedor"
+        verbose_name_plural = "proveedores"
+        managed = False
+
+class Pedido(models.Model):
+    id_pedido = models.AutoField(primary_key=True)
+    id_perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE, db_column='id_perfil')
+    id_bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE, db_column='id_bodega')
+    id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, db_column='id_proveedor')
+    fecha_pedido = models.CharField(max_length=30)
+    total_pedido = models.CharField(max_length=30)
+    publicaciones = models.ManyToManyField(Publicacion, through='Publicacion_Pedido')
+
+    # def __str__(self):
+        # return u'%s' % (self.nombre_genero)
+    
+    class Meta:
+        db_table = "pedido"
+        verbose_name_plural = "pedidos"
+        managed = False
+
+class Publicacion_Pedido(models.Model):
+    id_publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, db_column='id_publicacion')
+    id_pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, db_column='id_pedido')
+    cantidad = models.IntegerField(null=True)
+    precio_proveedor = models.IntegerField(null=True)
+
+    # def __str__(self):
+    #     return u'%s' % (self.nombre_genero)
+    
+    class Meta:
+        db_table = "publicacion_pedido"
+        verbose_name_plural = "publicacion_pedidos"
+        managed = False
+
+class Compra(models.Model):
+    id_compra = models.AutoField(primary_key=True)
+    id_bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE, db_column='id_bodega')
+    id_persona = models.ForeignKey(Persona, on_delete=models.CASCADE, db_column='id_persona')
+    total = models.IntegerField(null=True)
+    metodo_pago = models.CharField(max_length=30)
+    fecha_compra = models.CharField(max_length=30)
+    publicaciones = models.ManyToManyField(Publicacion, through='Publicacion_Compra')
+    
+    # def __str__(self):
+    #     return u'%s' % (self.nombre_genero)
+
+    class Meta:
+        db_table = "compra"
+        verbose_name_plural = "compras"
+        managed = False
+
+class Publicacion_Compra(models.Model):
+    id_publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, db_column='id_publicacion')
+    id_compra = models.ForeignKey(Compra, on_delete=models.CASCADE, db_column='id_compra')
+    cantidad = models.IntegerField(null=True)
+    precio = models.IntegerField(null=True)
+    
+    # def __str__(self):
+    #     return u'%s' % (self.nombre_genero)
+
+    class Meta:
+        db_table = "publicacion_compra"
+        verbose_name_plural = "publicacion_compras"
+        managed = False
+
+class Movimiento(models.Model):
+    id_movimiento = models.AutoField(primary_key=True)
+    id_perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE, db_column='id_perfil')
+    id_bodega_origen = models.ForeignKey(Bodega, on_delete=models.CASCADE, db_column='id_bodega_origen', related_name='bodega_origen')
+    id_bodega_destino = models.ForeignKey(Bodega, on_delete=models.CASCADE, db_column='id_bodega_destino', related_name='bodega_destino')
+    fecha_solicitud = models.CharField(max_length=30)
+    estado = models.CharField(max_length=30)
+    fecha_realizado = models.CharField(max_length=30)
+    publicaciones = models.ManyToManyField(Publicacion, through='Publicacion_Movimiento')
+
+    # def __str__(self):
+    #     return u'%s' % (self.nombre_genero)
+    
+    class Meta:
+        db_table = "movimiento"
+        verbose_name_plural = "movimientos"
+        managed = False
+
+class Publicacion_Movimiento(models.Model):
+    id_movimiento = models.ForeignKey(Movimiento, on_delete=models.CASCADE, db_column='id_movimiento')
+    id_publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, db_column='id_publicacion')
+    cantidad = models.IntegerField(null=True)
+
+    # def __str__(self):
+    #     return u'%s' % (self.nombre_genero)
+    
+    class Meta:
+        db_table = "publicacion_movimiento"
+        verbose_name_plural = "publicacion_movimientos"
         managed = False
