f89805672764fa36a3b7dfca46104adbc1431ddb
Jose Valencia <19josevalencia97@gmail.com>
2022-07-02 02:45:42 -0400

    actualización urls

--- libreria/inventario/urls.py
+++ libreria/inventario/urls.py
@@ -8,20 +8,14 @@ urlpatterns = [
     path('autores/registro', views.registroAutores, name='registroAutores'),
     path('productos', views.productos, name='productos'),
     path('productos/registro', views.registroProductos, name='registroProductos'),
-    path('bodegas', views.bodegas, name='bodegas'),
-    path('bodegas/<int:bodega_id>', views.detalleBodegas, name='detalleBodegas'),
-    path('bodegas/registro', views.registroBodegas, name='registroBodegas'),
     path('editoriales', views.editoriales, name='editoriales'),
     path('editoriales/registro', views.registroEditoriales, name='registroEditoriales'),
     path('personas/registro', views.registroPersonas, name='registroPersonas'),
     path('personas/lista', views.listaPersonas, name='listaPersonas'),
     path('personas/detalle/<int:id_persona>', views.detallePersonas, name='detallePersonas'),
-    path('proveedores/registro', views.registroProveedores, name='registroProveedores'),
     path('seed', views.seed, name='seed'),
     path('personas/editar/<int:id_persona>', views.editarPersonas, name='editarPersonas'),
-    path('proveedores', views.proveedores, name='proveedores'),
     path('login', views.loginPerfil, name='login'),
-    path('compras',views.listaCompras, name='compras'),
     path('perfiles', views.perfiles, name='perfiles'),
     path('perfiles/registro', views.registroPerfil, name='registroPerfil'),
     path('perfiles/preregistro', views.preregistroPerfil, name='preregistroPerfil'),
@@ -30,6 +24,12 @@ urlpatterns = [
     path('perfiles/cambiarEstado/<int:id_perfil>', views.cambiarEstadoPerfil, name='cambiarEstadoPerfil'),
     # URLS Bruno Pozo
     path('compras/registro',views.registroCompras,name='registroCompras'),
+    path('compras',views.listaCompras, name='compras'),
+    path('bodegas', views.bodegas, name='bodegas'),
+    path('bodegas/<int:bodega_id>', views.detalleBodegas, name='detalleBodegas'),
+    path('bodegas/registro', views.registroBodegas, name='registroBodegas'),
     # path('compras/detalles',views.detalleCompras,)
     # URLS Pablo Cea
+    path('proveedores/registro', views.registroProveedores, name='registroProveedores'),
+    path('proveedores', views.proveedores, name='proveedores'),
 ]
